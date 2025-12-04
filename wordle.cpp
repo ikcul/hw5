8f3c35003cb96336fb9dd0e747e7ef322a58a6dd
Daniel <simdaniel06@gmail.com>
2025-12-04 14:24:35 -0800

    wordle attempt

--- wordle.cpp
+++ wordle.cpp
@@ -22,7 +22,53 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> answerBank;
+    std::string temp = in; 
+    bool temp = wordleHelper(in, floating, dict, 0, 0, answerBank, temp);
+    return answerBank;
+    
 }
 
 // Define any helper functions here
+bool wordleHelper(const std::string& in, const std::string& floating, const std::set<std::string>& dict, int idxIn, int idxFloat, std::set<std::string>& answerBank, std::string& tempString){
+    //base case
+    if (idxIn >= in.size()){
+        return true;
+    }
+    //iterate through all possible options
+    if (in[idxIn] == '-'){
+        if (idxFloat < floating.size()){
+            tempString[idxIn] = floating[idxFloat];
+            bool status = wordleHelper(in, floating, dict, idxIn++, idxFloat++, answerBank, tempString);
+            if (status){
+                if (dict.find(tempString) != dict.end()){
+                    answerBank.insert(tempString);
+                }
+                return true;
+            }
+        }else{
+            for (int i = 0; i < 26; i++){
+                tempString[idxIn] = i + 'a';
+                bool status = wordleHelper(in, floating, dict, idxIn++, idxFloat++, answerBank, tempString);
+                if (status){
+                    //smth smth smth... tbd ig
+                    if (dict.find(tempString) != dict.end()){
+                        answerBank.insert(tempString);
+                    }
+                    return true;
+                }
+            }  
+        }
+    }else{
+        //skips if there is a letter already inserted and green
+        bool status = wordleHelper(in, floating, dict, idxIn++, idxFloat, answerBank, tempString);
+        if (status){
+            if (dict.find(tempString) != dict.end()){
+                answerBank.insert(tempString);
+            }
+            return true;
+        }
+    }
+    //if nothing works T_T hopefully never
+    return false;
+}
